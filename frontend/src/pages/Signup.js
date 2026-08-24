8e54f60853f6da6acd883de87c865aaf0a28fa4d
AYUSH KUMAR <yayush848@gmail.com>
2026-08-24 14:49:21 +0530

    Fix URL case in Signup API endpoint

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -31,7 +31,7 @@ function Signup() {
         }
 
         try{
-            const url = "https://deploy-auth-mern-app-7gwx-api.vercel.app/auth/Signup";
+            const url = "https://deploy-auth-mern-app-7gwx-api.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
