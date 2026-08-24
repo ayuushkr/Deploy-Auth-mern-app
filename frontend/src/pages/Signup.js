0b92b0338d81bb4484e2a9f4c8b27e3c093f2b36
AYUSH KUMAR <yayush848@gmail.com>
2026-08-24 12:47:22 +0530

    Update signup API URL to production endpoint

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -31,7 +31,7 @@ function Signup() {
         }
 
         try{
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://deploy-auth-mern-app-api.vercel.app/auth/Signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
